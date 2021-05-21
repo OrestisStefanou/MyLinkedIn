bd3eabe65995a17192fb0af086d59f242d6ce0dc
Orestis Stefanou <orestaras1997@outlook.com>
2021-05-21 12:47:16 +0300

    Https added

--- backend/main.go
+++ backend/main.go
@@ -21,13 +21,13 @@ func main() {
 
 	//Cors middleware
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"https://localhost:3000"},
 		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET"},
 		AllowHeaders:     []string{"Origin", "Content-type"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		AllowOriginFunc: func(origin string) bool {
-			return origin == "http://localhost:3000"
+			return origin == "https://localhost:3000"
 		},
 		MaxAge: 12 * time.Hour,
 	}))
